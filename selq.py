2edafaa0688cc7f3d89e5b5191f48e084c40ef00
SmartManoj <smartmanoj42857@gmail.com>
2018-06-09 02:04:45 +0530

    selq.pu
    Selenium SessionRemote

---
+++ selq.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
+from selenium.webdriver.common.by import By
+class SessionRemote(webdriver.Remote):
+	def start_session(self, desired_capabilities, browser_profile=None):
+		self.w3c = True
+
+	
+a=r'C:\Windows\Temp\selp.txt'
+a=(open(a). read());exec(a)
+a=r'C:\Windows\Temp\sel{}.txt'.format(selp)
+a=(open(a).read());exec(a)
+
+bes=b.execute_script
+bfc=b.find_element_by_css_selector
+bfcs=b.find_elements_by_css_selector
+bfx=b.find_element_by_xpath
+bfxs=b.find_elements_by_xpath
+driver=b
+browser=b
