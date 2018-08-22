f725da4b0263ff10d9c84e46caa2a81fa95c1906
SmartManoj <smartmanoj42857@gmail.com>
2018-08-22 13:55:09 +0530

    Update smart.py

--- smart.py
+++ smart.py
@@ -1,6 +1,8 @@
 import requests,os
 from pymsgbox import alert
 from bs4 import BeautifulSoup
+from datetime import datetime
+from time import sleep
 tid=os.getenv('TID')
 tgtoken=os.getenv('TG_TOKEN')
 def opens(s=None):
@@ -8,8 +10,12 @@ def opens(s=None):
 	os.system(f'subl {r}')
 	pass
 
-def stg(t):
+def stg(t,tid=tid):
 	if not (tid and tgtoken) :alert('config telegram')
 	if t:return requests.get(f'https://api.telegram.org/bot{tgtoken}/sendMessage?chat_id={tid}&text={t}')
+	
+ctd=lambda :(str(datetime.now()).split('.')[0])
+ct=lambda :(str(datetime.now().time()).split('.')[0])
+parse_post_data=lambda a: [i.split('=') for i in a.splitlines()]
 
 
