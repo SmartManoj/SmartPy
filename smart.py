3afa75072e2a88a5009e9e39802c0c3c2cf7f9ba
SmartManoj <smartmanoj42857@gmail.com>
2019-05-08 19:26:53 +0530

    Update smart.py

--- smart.py
+++ smart.py
@@ -1,15 +1,17 @@
-import requests,os
+import requests,os,sys
 from pymsgbox import alert
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime,timedelta
 from time import sleep
 tid=os.getenv('TID')
 tgtoken=os.getenv('TG_TOKEN')
 def opens(s=None):
+	if type(s)==str and s not in sys.modules:exec('import {s}')
 	r = s.__file__ if s	else __file__
 	os.system(f'subl {r}')
 	pass
-
+def done():
+	alert('Done',timeout=3)
 def stg(t,tid=tid):
 	if not (tid and tgtoken) :alert('config telegram')
 	if t:return requests.get(f'https://api.telegram.org/bot{tgtoken}/sendMessage?chat_id={tid}&text={t}')
@@ -20,3 +22,6 @@ parse_request_header=lambda x: [i.split(': ',1) for i in x.splitlines()[1:]]
 parse_post_data=lambda a: [i.split('=') for i in a.splitlines()] 
 
 
+if __name__ == '__main__':
+	import webbrowser
+	webbrowser.open()
